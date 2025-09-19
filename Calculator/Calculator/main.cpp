d342c1c28113888fc85f25fe67b2b6d8a284be4f
https://koger1O@bitbucket.org/SHOT/SHOT_Assignment1.git <https://[gergo_kocsis@outlook.com]@bitbucket.org/[SHOT]/[SHOT_assignment1].git>
2025-09-19 18:55:57 +0100

    Basic setup

---
+++ Calculator/Calculator/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <optional>
+#include <utility>
+#include <stack>
+
+std::optional<int> createExpression(const std::string& stringExpression, std::vector<std::string>& expression)
+{
+
+}
+
+int main()
+{
+	std::vector<std::pair<std::string, int>> testCases =
+	{
+		{"1+2", 3},
+	};
+
+	return 0;
+}
